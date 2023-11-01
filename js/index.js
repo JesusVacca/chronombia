c0b0e8e02acc34823c2008c27f1d1c62b02cf7c0
Jesus Vacca <jesus.vacca99@gmail.com>
2023-11-01 18:02:17 -0500

    actualizando lo responsive

--- js/index.js
+++ js/index.js
@@ -2,6 +2,6 @@ const hamburguesa = document.getElementById("hamburguesa");
 const menu_items = document.getElementById("menu-items")
 
 hamburguesa.onclick =()=>{
-    hamburguesa.classList.toggle("activo-haburguesa");
+    hamburguesa.classList.toggle("activo-hamburguesa");
     menu_items.classList.toggle("activo");
 }
